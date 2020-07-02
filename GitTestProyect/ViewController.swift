2e962b9b6bf3350fdcd8da6549d387f0a3155a9b
Saul Granados <saulegvit@gmail.com>
2020-07-02 11:51:47 -0600

    Initial Commit

---
+++ GitTestProyect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProyect
+//
+//  Created by Saul Granados on 7/2/20.
+//  Copyright © 2020 Saul Granados. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
